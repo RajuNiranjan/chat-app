6fba66e03cf83dea548c7db82386996af225bded
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-12-19 20:48:46 +0530

    removed all files

--- server/src/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { LogIn, LogOut, Register } from "../controllers/auth.controller.js";
-
-export const AuthRouter = express.Router();
-
-AuthRouter.post("/register", Register);
-AuthRouter.post("/login", LogIn);
-AuthRouter.post("/logout", LogOut);
